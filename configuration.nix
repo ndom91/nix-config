d29271cedc8b6ec222d5faaab46045b573212164
ndom91 <yo@ndo.dev>
2024-02-26 16:21:59 +0100

    fix: xkb.layout syntax fix

---
+++ configuration.nix
@@ -0,0 +1,201 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ lib, config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "ndo-nix"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.wayland = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    xkb = {
+      layout = "us";
+      variant = "";
+      options = "caps:escape";
+    };
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # systemd services
+  systemd.services.systemd-udevd.restartIfChanged = false;
+  systemd.services.NetworkManager-wait-online.enable = false;
+  systemd.services.systemd-networkd-wait-online.enable = false;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  users.users.ndo = {
+    isNormalUser = true;
+    description = "ndo";
+    extraGroups = [ "networkmanager" "docker" "wheel" "libvirt" "kvm" ];
+    packages = with pkgs; [
+      vivaldi
+      gnome.gnome-boxes
+      slack
+      virt-manager
+    ];
+  };
+
+
+  nixpkgs.config.allowUnfree = true;
+
+  # environment.systemPackages = import ./common-packages.nix {
+    # pkgs = pkgs;
+  # };
+
+  environment.systemPackages = with pkgs; [
+    ## Unstable
+    # TODO: Needs setup
+    # unstable.neovim
+    neovim
+
+    # alejandra # nix code formatter
+    coreutils
+    difftastic
+    docker-compose
+    dua
+    fd
+    ffmpeg
+    git
+    htop
+    ipmitool
+    jq
+    lm_sensors
+    neofetch
+    nmap
+    ouch
+    qemu
+    ripgrep
+    smartmontools
+    # terraform
+    tmux
+    tree
+    tree
+    watch
+    wget
+  ];
+
+  # FOR LATER: dynamically-linked binaries work-around
+  # programs.nix-ld.enable = true;
+  # programs.nix-ld.libraries with pkgs; [
+    # Add any missing dynamic libraries for unpackaged programs
+    # here, NOT in environment.systemPackages
+  # ];
+
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = true;
+    settings.PermitRootLogin = "yes";
+  };
+
+  services.fwupd.enable = true;
+  services.tailscale.enable = true;
+
+  virtualisation.libvirtd.enable = true;
+  virtualisation.spiceUSBRedirection.enable = true;
+  virtualisation = {
+    docker = {
+      enable = true;
+      autoPrune = {
+        enable = true;
+        dates = "weekly";
+      };
+    };
+  };
+
+  nix = {
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      warn-dirty = false;
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+
+  # system.copySystemConfiguration = true;
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+}
+
+## hardware-configuration.nix options for future
+# networking.useDHCP = lib.mkDefault true;
+# nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+# hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+# Bluetooth
+# hardware.bluetooth.enable = true;
+# hardware.bluetooth.powerOnBoot = true;
