8ff2a813f7f63f6dbc14152ae35a3c48ef31b594
ndom91 <yo@ndo.dev>
2024-03-02 04:52:59 +0100

    fix: move swayidle service under home-manager

--- hosts/ndo2/home.nix
+++ hosts/ndo2/home.nix
@@ -88,6 +88,30 @@
       #   enable = true;
       # };
     };
+    swayidle = {
+      enable = true;
+      timeouts = [
+        {
+          timeout = 295;
+          command = "${pkgs.libnotify}/bin/notify-send 'Locking in 5 seconds' -t 5000";
+        }
+        {
+          timeout = 300;
+          command = "${pkgs.swaylock}/bin/swaylock";
+        }
+        {
+          timeout = 360;
+          command = "${pkgs.hyprland}/bin/hyprctl dispatch dpms off";
+          resumeCommand = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
+        }
+      ];
+      events = [
+        {
+          event = "before-sleep";
+          command = "${pkgs.swaylock}/bin/swaylock";
+        }
+      ];
+    };
   };
 
   programs.atuin = {
