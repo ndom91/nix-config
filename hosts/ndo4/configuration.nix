25941c9b47b090251a24d71a1a79edabe9d53f0f
ndom91 <yo@ndo.dev>
2024-03-07 13:21:16 +0100

    fix: bump nixpkgs cachix priority

--- hosts/ndo4/configuration.nix
+++ hosts/ndo4/configuration.nix
@@ -15,13 +15,13 @@ in
     settings = {
       auto-optimise-store = true;
       substituters = [
+        "https://cache.nixos.org?priority=10"
         "https://hyprland.cachix.org"
-        "https://cache.nixos.org/"
         "https://nix-community.cachix.org"
       ];
       trusted-public-keys = [
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
       experimental-features = [ "nix-command" "flakes" ];
