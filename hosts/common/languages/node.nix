3db903cdf917b032a3a687bd7e4681e9fdc21460
ndom91 <yo@ndo.dev>
2024-03-08 16:00:21 +0100

    fix: prisma env vars

--- hosts/common/languages/node.nix
+++ hosts/common/languages/node.nix
@@ -1,6 +1,15 @@
-{ pkgs, input, ... }:
+{ pkgs, input, unstablePkgs, ... }:
 {
   home.packages = with pkgs; [
     nodejs_20
+    unstablePkgs.nodePackages.pnpm
+    openssl
+    prisma-engines
   ];
+
+  # Prisma:
+  programs.bash.sessionVariables.PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
+  programs.bash.sessionVariables.PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
+  programs.bash.sessionVariables.PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
+  programs.bash.sessionVariables.PRISMA_ENGINES_CHECKSUM_IGNORE_MISSING = "1";
 }
