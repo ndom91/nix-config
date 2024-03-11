55bfc8fe3f6d1a6ab90895f535ecb0281043ddd0
ndom91 <yo@ndo.dev>
2024-03-11 17:48:38 +0100

    feat: add agenix secrets

--- modules/home-manager/ssh.nix
+++ modules/home-manager/ssh.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, config, ... }:
 {
   programs.ssh = {
     enable = true;
@@ -7,52 +7,6 @@
         StrictHostKeyChecking no
         SetEnv TERM=xterm-256color
     '';
-    matchBlocks = {
-      github.com = {
-        hostname = "github.com";
-        user = "git";
-        identityFile = "~/.ssh/id_ndo4";
-      };
-      ndo2 = {
-        hostname = "ndo2.puff.lan";
-        user = "ndo";
-        identityFile = "~/.ssh/id_ndo4";
-      };
-      ndo4 = {
-        hostname = "ndo4.puff.lan";
-        user = "ndo";
-        identityFile = "~/.ssh/id_ndo4";
-      };
-      tursteher = {
-        hostname = "tursteher.puff.lan";
-        user = "ndo";
-        identityFile = "~/.ssh/id_ndo4";
-      };
-      hassio = {
-        hostname = "hassio.puff.lan";
-        user = "ndo";
-        identityFile = "~/.ssh/id_ndo4";
-      };
-      usbx = {
-        hostname = "netbox.puff.lan";
-        user = "ndo";
-        identityFile = "~/.ssh/id_ndo4";
-      };
-      netbox = {
-        hostname = "netbox.puff.lan";
-        user = "ndo";
-        identityFile = "~/.ssh/id_ndo4";
-      };
-      paperless = {
-        hostname = "paperless.puff.lan";
-        user = "ndo";
-        identityFile = "~/.ssh/id_ndo4";
-      };
-      zuhalter = {
-        hostname = "zuhalter.puff.lan";
-        user = "ndo";
-        identityFile = "~/.ssh/id_ndo4";
-      };
-    };
+    matchBlocks = config.age.secrets.secret1.path;
   };
 }
