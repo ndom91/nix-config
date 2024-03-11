313b06feebc29e59849635f86dae1839ced2e8bc
ndom91 <yo@ndo.dev>
2024-03-11 18:45:23 +0100

    fix: syntax nginx config

--- modules/nixos/nginx.nix
+++ modules/nixos/nginx.nix
@@ -8,17 +8,19 @@
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
 
-    virtualHosts."db.puff.lan" = {
-      listen = [{
-        port = 5432;
-      }];
-      upstreams = {
-        docker-postgres = {
-          servers = {
-            "docker-pi.puff.lan:5432" = { };
-          };
+    upstreams = {
+      docker-postgres = {
+        servers = {
+          "docker-pi.puff.lan:5432" = { };
         };
       };
+    };
+
+    virtualHosts."db.puff.lan" = {
+      listen = [{
+        addr = "127.0.0.1";
+        port = 5432;
+      }];
       locations."/" = {
         proxyPass = "docker-postgres";
       };
