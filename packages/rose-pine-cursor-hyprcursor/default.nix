677324ff5c9e8e2c7a26277efa6281dc15d9d7ad
ndom91 <yo@ndo.dev>
2024-03-12 16:16:03 +0100

    feat: fix rose-pine-cursor-hyprcursor derivation

--- packages/rose-pine-cursor-hyprcursor/default.nix
+++ packages/rose-pine-cursor-hyprcursor/default.nix
@@ -1,15 +1,13 @@
-{ lib, stdenvNoCC, fetchurl }:
+{ lib, stdenv, pkgs, fetchurl }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "rose-pine-cursor-hyprcursor";
   version = "0.1.0";
-  src = fetchFromGitHub {
+  src = pkgs.fetchFromGitHub {
     owner = "ndom91";
     repo = "rose-pine-cursor-hyprcursor";
-    rev = "f96a8e09fa7e60d5e48543e15693774dcf092fbe";
-    sha256 = "sha256-CPK3kbc8lroPU8MAeNP8JSStzDCKCvAHhj6yQ1fWKkY=";
+    rev = "e4dde6d19da2d048cf4b224fe2ed1576c9c50087";
+    sha256 = "sha256-h7qivaCqoJj1ZbWT86WA1gp769vgTUhDDxcJOZ1WfMU=";
   };
-
-  # sourceRoot = ".";
 
   installPhase = ''
     mkdir -p $out/share/icons/rose-pine-cursor-hyprcursor
