6513ef263ce53988c9e3f2e9c36b0ee7b6eb1365
ndom91 <yo@ndo.dev>
2024-03-20 15:05:33 +0100

    fix: bump linux kernel to latest

--- hosts/ndo2/hardware-configuration.nix
+++ hosts/ndo2/hardware-configuration.nix
@@ -7,6 +7,7 @@
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
