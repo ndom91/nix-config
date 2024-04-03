6c96ddbe7fd0c113ec50fc66ecf551aba1ca4039
ndom91 <yo@ndo.dev>
2024-04-03 14:29:20 +0200

    fix: bump hyprland and neovim flake

--- modules/home-manager/wayland/hyprland/default.nix
+++ modules/home-manager/wayland/hyprland/default.nix
@@ -25,7 +25,8 @@
       # (unstablePkgs.hyprlandPlugins.hy3.override {
       #   hyprland = inputs.hyprland.packages.${pkgs.system}.hyprland;
       # })
-      inputs.hyprfocus.packages.${pkgs.system}.hyprfocus
+      # Hyprfocus compilation broken until: https://github.com/pyt0xic/hyprfocus/pull/1 merged
+      # inputs.hyprfocus.packages.${pkgs.system}.hyprfocus
     ];
 
     settings = {
@@ -395,34 +396,34 @@
         #     trigger_height = 500
         #   }
         # }
-        hyprfocus {
-          enabled = yes
-
-          focus_animation = shrink
-
-          bezier = bezIn, 0.5,0.0,1.0,0.5
-          bezier = bezOut, 0.0,0.5,0.5,1.0
-
-          flash {
-              flash_opacity = 0.8
-
-              in_bezier = bezIn
-              in_speed = 0.5
-
-              out_bezier = bezOut
-              out_speed = 3
-          }
-
-          shrink {
-            shrink_percentage = 0.995
-
-            in_bezier = bezIn
-            in_speed = 0.25
-
-            out_bezier = bezOut
-            out_speed = 1
-          }
-        }
+        # hyprfocus {
+        #   enabled = yes
+        #
+        #   focus_animation = shrink
+        #
+        #   bezier = bezIn, 0.5,0.0,1.0,0.5
+        #   bezier = bezOut, 0.0,0.5,0.5,1.0
+        #
+        #   flash {
+        #       flash_opacity = 0.8
+        #
+        #       in_bezier = bezIn
+        #       in_speed = 0.5
+        #
+        #       out_bezier = bezOut
+        #       out_speed = 3
+        #   }
+        #
+        #   shrink {
+        #     shrink_percentage = 0.995
+        #
+        #     in_bezier = bezIn
+        #     in_speed = 0.25
+        #
+        #     out_bezier = bezOut
+        #     out_speed = 1
+        #   }
+        # }
       }
     '';
   };
