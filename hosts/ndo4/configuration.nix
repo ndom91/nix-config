626b5535d30a90d3c8709bf809f63edc534e204d
ndom91 <yo@ndo.dev>
2024-04-12 17:59:58 +0200

    fix: update ndo4 hosts file

--- hosts/ndo4/configuration.nix
+++ hosts/ndo4/configuration.nix
@@ -116,6 +116,7 @@ in
     hosts = {
       "127.0.0.1" = [ "localhost" "ndo4" "sveltekasten" "db.puff.lan" ];
       "10.0.0.25" = [ "checkly.pi" "docker-pi" ];
+      "10.0.0.52" = [ "netbox.puff.lan" "paperless.puff.lan" ];
     };
   };
 
