60f9c0fa02d8138884eeb5ac043cafadcbcf5707
ndom91 <yo@ndo.dev>
2024-06-08 18:05:55 +0200

    feat: use 1.44.0 playwright from PR

--- flake.nix
+++ flake.nix
@@ -1,22 +1,19 @@
 {
   description = "ndom91 config flake";
   inputs = {
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
 
-    # hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1&rev=cba1ade848feac44b2eda677503900639581c3f4";
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+    # "git+https://github.com/hyprwm/Hyprland?submodules=1&rev=cba1ade848feac44b2eda677503900639581c3f4";
     hyprfocus = {
       url = "github:pyt0xic/hyprfocus";
       inputs.hyprland.follows = "hyprland";
     };
     hy3 = {
-      url = "github:outfoxxed/hy3"; # where {version} is the hyprland release version
+      url = "github:outfoxxed/hy3";
       # url = "github:outfoxxed/hy3?ref=hl{version}"; # where {version} is the hyprland release version
-      # or "github:outfoxxed/hy3" to follow the development branch.
-      # (you may encounter issues if you dont do the same for hyprland)
       inputs.hyprland.follows = "hyprland";
     };
     rose-pine-hyprcursor.url = "github:ndom91/rose-pine-hyprcursor";
@@ -35,20 +32,28 @@
       inputs.darwin.follows = "";
     };
     home-manager = {
-      # url = "github:nix-community/home-manager/release-23.11";
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=v0.1.0";
-    superfile = {
-      url = "github:MHNightCat/superfile";
-    };
+    superfile.url = "github:MHNightCat/superfile";
+    playwright.url = "github:kalekseev/nixpkgs/playwright-core";
   };
 
   outputs = { self, unstable, agenix, nix-colors, nixpkgs, ... } @inputs:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      # pkgs = nixpkgs.legacyPackages.${system};
+
+      playwrightOverlay = final: prev: {
+        inherit (inputs.playwright.packages.${prev.system})
+          playwright-driver playwright-test;
+      };
+
+      pkgs = import nixpkgs {
+        system = system;
+        overlays = [ playwrightOverlay ];
+      };
       unstablePkgs = unstable.legacyPackages.${system};
     in
     {
--- modules/home-manager/languages/node.nix
+++ modules/home-manager/languages/node.nix
@@ -1,4 +1,4 @@
-{ pkgs, input, unstablePkgs, ... }:
+{ pkgs, inputs, unstablePkgs, ... }:
 {
   home.packages = with pkgs; [
     nodejs_20
@@ -7,7 +7,8 @@
     unstablePkgs.bun
     openssl
     pkg-config
-    unstablePkgs.playwright-driver
+
+    pkgs.playwright-driver
     vips # for sharp
     unstablePkgs.prisma-engines
     unstablePkgs.chromium
@@ -21,7 +22,8 @@
     PRISMA_ENGINES_CHECKSUM_IGNORE_MISSING = "1";
 
     # Playwright
-    PLAYWRIGHT_BROWSERS_PATH = unstablePkgs.playwright-driver.browsers;
+    PLAYWRIGHT_BROWSERS_PATH = pkgs.playwright-driver.browsers;
+    PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD = 1;
     PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = true;
   };
 }
