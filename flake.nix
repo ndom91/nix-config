18f6862bc56c804fccf391d37a03f0d2358e5770
ndom91 <yo@ndo.dev>
2024-06-11 22:03:19 +0200

    fix: bump hyprland@0.41.0

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
 
-    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1&tag=v0.41.0";
     # "git+https://github.com/hyprwm/Hyprland?submodules=1&rev=cba1ade848feac44b2eda677503900639581c3f4";
     hyprfocus = {
       url = "github:pyt0xic/hyprfocus";
