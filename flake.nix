ac99f201beb14ecf5d78431906ffb696b47c5361
ndom91 <yo@ndo.dev>
2024-06-15 13:49:45 +0200

    feat: bump hyprland/hy3 to latest versions

--- flake.nix
+++ flake.nix
@@ -5,18 +5,20 @@
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
 
-    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1&rev=ea2501d4556f84d3de86a4ae2f4b22a474555b9f";
-    # hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1&tag=v0.41.0";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1&tag=v0.41.1";
+    # hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+    # hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1&rev=ea2501d4556f84d3de86a4ae2f4b22a474555b9f";
     # "git+https://github.com/hyprwm/Hyprland?submodules=1&rev=cba1ade848feac44b2eda677503900639581c3f4";
-    hyprfocus = {
-      url = "github:pyt0xic/hyprfocus";
-      inputs.hyprland.follows = "hyprland";
-    };
     hy3 = {
-      url = "github:outfoxxed/hy3?ref=hl0.41.0";
+      url = "github:outfoxxed/hy3";
+      # url = "github:outfoxxed/hy3?ref=hl0.41.0";
       # url = "github:outfoxxed/hy3?ref=hl{version}"; # where {version} is the hyprland release version
       inputs.hyprland.follows = "hyprland";
     };
+    # hyprfocus = {
+    #   url = "github:pyt0xic/hyprfocus";
+    #   inputs.hyprland.follows = "hyprland";
+    # };
     rose-pine-hyprcursor.url = "github:ndom91/rose-pine-hyprcursor";
     apple-fonts.url = "github:Lyndeno/apple-fonts.nix";
 
--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -44,34 +44,34 @@ in
     prefix = "C-a";
     plugins = [
       # tmux-window-name
-      {
-        plugin = unstablePkgs.tmuxPlugins.catppuccin;
-        extraConfig = ''
-          set -g @catppuccin_flavour 'mocha'
-
-          # set -g @catppuccin_user off
-          # set -g @catppuccin_host off
-          set -g @catppuccin_date_time "%Y-%m-%d %H:%M"
-
-          set -g @catppuccin_window_left_separator "█"
-          set -g @catppuccin_window_right_separator "█"
-          set -g @catppuccin_window_number_position "right"
-          set -g @catppuccin_window_middle_separator "  █"
-          set -g @catppuccin_window_status_enable "no"
-          set -g @catppuccin_window_default_fill "number"
-          set -g @catppuccin_window_default_text "#W"
-          set -g @catppuccin_window_current_fill "number"
-          set -g @catppuccin_window_current_text "#W"
-
-          set -g @catppuccin_status_modules_left ""
-          set -g @catppuccin_status_modules_right "host prefix_highlight"
-          set -g @catppuccin_status_left_separator "█"
-          set -g @catppuccin_status_right_separator "█ "
-          set -g @catppuccin_status_right_separator_inverse "no"
-          set -g @catppuccin_status_fill "all"
-          set -g @catppuccin_status_connect_separator "no"
-        '';
-      }
+      # {
+      #   plugin = unstablePkgs.tmuxPlugins.catppuccin;
+      #   extraConfig = ''
+      #     set -g @catppuccin_flavour 'mocha'
+      #
+      #     # set -g @catppuccin_user off
+      #     # set -g @catppuccin_host off
+      #     set -g @catppuccin_date_time "%Y-%m-%d %H:%M"
+      #
+      #     set -g @catppuccin_window_left_separator "█"
+      #     set -g @catppuccin_window_right_separator "█"
+      #     set -g @catppuccin_window_number_position "right"
+      #     set -g @catppuccin_window_middle_separator "  █"
+      #     set -g @catppuccin_window_status_enable "no"
+      #     set -g @catppuccin_window_default_fill "number"
+      #     set -g @catppuccin_window_default_text "#W"
+      #     set -g @catppuccin_window_current_fill "number"
+      #     set -g @catppuccin_window_current_text "#W"
+      #
+      #     set -g @catppuccin_status_modules_left ""
+      #     set -g @catppuccin_status_modules_right "host prefix_highlight"
+      #     set -g @catppuccin_status_left_separator "█"
+      #     set -g @catppuccin_status_right_separator "█ "
+      #     set -g @catppuccin_status_right_separator_inverse "no"
+      #     set -g @catppuccin_status_fill "all"
+      #     set -g @catppuccin_status_connect_separator "no"
+      #   '';
+      # }
       {
         plugin = unstablePkgs.tmuxPlugins.rose-pine;
         extraConfig = ''
