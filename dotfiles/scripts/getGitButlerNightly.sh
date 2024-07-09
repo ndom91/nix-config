3459791c17dccc37b88d16328bba810bf31e20a6
ndom91 <yo@ndo.dev>
2024-07-09 18:05:30 +0200

    feat: add gitButler nightly download script

--- dotfiles/scripts/getGitButlerNightly.sh
+++ dotfiles/scripts/getGitButlerNightly.sh
@@ -1,28 +1,46 @@
 #!/usr/bin/env bash
 
-CYAN='\033[1;36m'
-NC='\033[0m'
+# set -x
+CYAN="\e[0;96m"
+BOLDCYAN="\e[1;96m"
+CYANBG="\e[46;1m"
+RED="\e[0;91m"
+NC="\e[0m"
 
-appImageDir="/opt/appimages"
-version="$1"
-buildId="$2"
+# Default URL to latest AppImage
+URL=$(curl -sk https://app.gitbutler.com/releases/nightly | jq '.platforms."linux-x86_64".url' | sed 's|\"||g')
 
-if [[ -z "$version" || -z "$buildId" ]]; then
-  echo -e "[${CYAN}*${NC}] Usage: $0 <version> <buildId>"
+# Allow overriding via first argument
+if [[ $1 ]]; then
+ URL="$1"
+fi
+
+TARGET_DIR="/opt/appimages/"
+TARBALL_FILENAME=$(basename "$URL")
+APPIMAGE_FILENAME=$(basename --suffix=".tar.gz"  "$URL")
+
+echo -e "\n  ${BOLDCYAN}⧑${NC}  ${CYANBG} GitButler ${NC} Nightly Downloader\n"
+
+cd "$TARGET_DIR" || return
+
+echo -e "[${CYAN}*${NC}] Downloading ${CYAN}$TARBALL_FILENAME${NC}"
+
+if ! wget --quiet "$URL"; then
+  echo -e "Download failed - ${RED}$URL${NC}"
   exit 1
 fi
 
-echo -e "[${CYAN}*${NC}] Downloading nightly release ${CYAN}$version-$buildId${NC}"
+echo -e "[${CYAN}*${NC}] Extracting ${CYAN}$TARBALL_FILENAME${NC}"
 
-wget \
-  -cqO "$appImageDir/git-butler-nightly_latest.tar.gz" \
-  "https://releases.gitbutler.com/releases/nightly/$version-$buildId/linux/x86_64/git-butler-nightly_${version}_amd64.AppImage.tar.gz"
-
-echo -e "[${CYAN}*${NC}] Extracting"
-
-tar -xf "$appImageDir/git-butler-nightly_latest.tar.gz"
+tar -xf "$TARBALL_FILENAME"
 
 echo -e "[${CYAN}*${NC}] Cleaning up"
 
-rm "$appImageDir/git-butler-nightly_latest.tar.gz"
-mv "$appImageDir/git-butler-nightly_${version}_amd64.AppImage" "$appImageDir/git-butler-nightly_latest.AppImage"
+rm -f /opt/appimages/git-butler-nightly_latest.AppImage
+rm -f -- *.tar.gz
+
+echo -e "[${CYAN}*${NC}] Linking latest AppImage"
+
+ln -s "$APPIMAGE_FILENAME" git-butler-nightly_latest.AppImage
+
+echo -e "[${CYAN}*${NC}] Complete 🎉"
