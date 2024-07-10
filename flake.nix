cd415988bb417a266fbcc273cc92e5f325408364
ndom91 <yo@ndo.dev>
2024-07-10 12:15:23 +0200

    fix: cleanup flake.nix

--- flake.nix
+++ flake.nix
@@ -6,27 +6,7 @@
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
 
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1&tag=v0.41.2";
-    # hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
-    # hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1&rev=ea2501d4556f84d3de86a4ae2f4b22a474555b9f";
-    # "git+https://github.com/hyprwm/Hyprland?submodules=1&rev=cba1ade848feac44b2eda677503900639581c3f4";
-    # hy3 = {
-      # url = "github:outfoxxed/hy3";
-      # url = "github:outfoxxed/hy3?ref=hl0.41.0";
-      # url = "github:outfoxxed/hy3?ref=hl{version}"; # where {version} is the hyprland release version
-      # inputs.hyprland.follows = "hyprland";
-    # };
-    # hyprfocus = {
-    #   url = "github:pyt0xic/hyprfocus";
-    #   inputs.hyprland.follows = "hyprland";
-    # };
     rose-pine-hyprcursor.url = "github:ndom91/rose-pine-hyprcursor";
-    # apple-fonts.url = "github:Lyndeno/apple-fonts.nix";
-
-    # hyprland-plugins = {
-    #   url = "github:hyprwm/hyprland-plugins";
-    #   inputs.hyprland.follows = "hyprland";
-    # };
-    # nix-gaming.url = "github:fufexan/nix-gaming";
 
     nix-colors.url = "github:misterio77/nix-colors";
     agenix = {
