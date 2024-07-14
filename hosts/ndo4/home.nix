5ce6f80a54db101846cd2d2de456891568969997
ndom91 <yo@ndo.dev>
2024-07-14 15:57:47 +0200

    fix: refactor common home-manager settings out

--- hosts/ndo4/home.nix
+++ hosts/ndo4/home.nix
@@ -2,33 +2,18 @@
 {
   imports = with rose-pine-cursor inputs pkgs unstablePkgs; [
     nix-colors.homeManagerModules.default
-    # User Packages
-    ../../modules/home-manager/user-packages.nix
-    # Programming Lanaguages
-    ../../modules/home-manager/languages/node.nix
-    ../../modules/home-manager/languages/rust.nix
-    # Common
-    ../../modules/home-manager/git.nix
-    ../../modules/home-manager/tmux.nix
-    ../../modules/home-manager/neofetch.nix
-    ../../modules/home-manager/bash.nix
-    ../../modules/home-manager/xdg.nix
-    ../../modules/home-manager/dconf.nix
-    ../../modules/home-manager/gtk/default.nix
-    ../../modules/home-manager/starship.nix
-    ../../modules/home-manager/neovim.nix
-    ../../modules/home-manager/zathura.nix
-    ../../modules/home-manager/wezterm.nix
-    ../../modules/home-manager/alacritty.nix
-    ../../modules/home-manager/wayland/default.nix
+    ../../modules/home-manager/default.nix
   ];
-  home.username = "ndo";
-  home.homeDirectory = "/home/ndo";
-  home.stateVersion = stateVersion;
-  home.packages = [
-    inputs.home-manager.packages.x86_64-linux.home-manager # home-manager binary
-    fira-sans-nerd-font
-  ];
+
+  home = {
+    username = "ndo";
+    homeDirectory = "/home/ndo";
+    stateVersion = stateVersion;
+    packages = [
+      inputs.home-manager.packages.x86_64-linux.home-manager # home-manager binary
+      fira-sans-nerd-font
+    ];
+  };
 
   # systemd.user.sessionVariables = config.home.sessionVariables;
   systemd.user.startServices = "sd-switch";
@@ -54,71 +39,9 @@
     };
   };
 
-  home.file = {
-    ".ripgreprc".source = ../../dotfiles/.ripgreprc;
-    ".local/share/fonts" = {
-      recursive = true;
-      source = ./../../dotfiles/fonts;
-    };
-
-    ".config/hypr/wallpaper.png".source = ../../dotfiles/wallpapers/dark-purple-space-01.png;
-
-    ".config/brave-flags.conf".source = ../../dotfiles/brave-flags.conf;
-    ".config/electron-flags.conf".source = ../../dotfiles/electron-flags.conf;
-  };
-
   services = {
     network-manager-applet.enable = false;
-    syncthing.enable = true;
-  };
-
-  programs.atuin = {
-    enable = true;
-    settings = {
-      auto_sync = false;
-      style = "compact";
-      history_filter = [
-        "^cd"
-        "^ll"
-        "^n?vim"
-      ];
-      common_prefix = [ "sudo" "ll" "cd" "clear" "ls" "echo" "pwd" "exit" "history" ];
-      secrets_filter = true;
-      enter_accept = true;
-    };
-  };
-
-  programs.vscode = {
-    enable = true;
-    package = unstablePkgs.vscodium;
-    extensions = with unstablePkgs.vscode-extensions; [
-      mvllow.rose-pine
-      vscodevim.vim
-      esbenp.prettier-vscode
-      svelte.svelte-vscode
-      rust-lang.rust-analyzer
-    ];
-  };
-
-  programs.direnv = {
-    enable = true;
-    enableBashIntegration = true;
-    nix-direnv.enable = true;
-  };
-
-  # Mostly for use with comma
-  programs.nix-index = {
-    enable = true;
-    enableBashIntegration = true;
-    package = pkgs.nix-index;
   };
 
   programs.home-manager.enable = true;
-  programs.bash.enable = true;
-  programs.zoxide = {
-    enable = true;
-    options = [
-      "--cmd cd"
-    ];
-  };
 }
---
+++ modules/home-manager/default.nix
@@ -0,0 +1,84 @@
+{ fira-sans-nerd-font, rose-pine-cursor, nix-colors, lib, inputs, stateVersion, config, pkgs, unstablePkgs, ... }:
+{
+  imports = with rose-pine-cursor inputs pkgs unstablePkgs; [
+    # User Packages
+    ../../modules/home-manager/user-packages.nix
+    # Programming Lanaguages
+    ../../modules/home-manager/languages/node.nix
+    ../../modules/home-manager/languages/rust.nix
+    # Common
+    ../../modules/home-manager/git.nix
+    ../../modules/home-manager/tmux.nix
+    ../../modules/home-manager/neofetch.nix
+    ../../modules/home-manager/bash.nix
+    ../../modules/home-manager/xdg.nix
+    ../../modules/home-manager/dconf.nix
+    ../../modules/home-manager/gtk/default.nix
+    ../../modules/home-manager/starship.nix
+    ../../modules/home-manager/neovim.nix
+    ../../modules/home-manager/zathura.nix
+    ../../modules/home-manager/wezterm.nix
+    ../../modules/home-manager/alacritty.nix
+    ../../modules/home-manager/wayland/default.nix
+  ];
+
+  home.file = {
+    ".ripgreprc".source = ../../dotfiles/.ripgreprc;
+    ".local/share/fonts" = {
+      recursive = true;
+      source = ./../../dotfiles/fonts;
+    };
+
+    ".config/hypr/wallpaper.png".source = ../../dotfiles/wallpapers/dark-purple-space-01.png;
+
+    ".config/brave-flags.conf".source = ../../dotfiles/brave-flags.conf;
+    ".config/electron-flags.conf".source = ../../dotfiles/electron-flags.conf;
+  };
+
+  services = {
+    syncthing.enable = true;
+  };
+
+  programs.bash.enable = true;
+
+  programs.atuin = {
+    enable = true;
+    settings = {
+      auto_sync = false;
+      style = "compact";
+      history_filter = [
+        "^cd"
+        "^ll"
+        "^n?vim"
+      ];
+      common_prefix = [ "sudo" "ll" "cd" "clear" "ls" "echo" "pwd" "exit" "history" ];
+      secrets_filter = true;
+      enter_accept = true;
+    };
+  };
+
+  programs.vscode = {
+    enable = true;
+    package = unstablePkgs.vscodium;
+    extensions = with unstablePkgs.vscode-extensions; [
+      mvllow.rose-pine
+      vscodevim.vim
+      esbenp.prettier-vscode
+      svelte.svelte-vscode
+      rust-lang.rust-analyzer
+    ];
+  };
+
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true;
+    nix-direnv.enable = true;
+  };
+
+  programs.zoxide = {
+    enable = true;
+    options = [
+      "--cmd cd"
+    ];
+  };
+}
