c9a3ed0b1fc915146321632e942dd699f6153dce
ndom91 <yo@ndo.dev>
2024-10-01 16:31:12 +0200

    fix: vim treesitter workarounds

--- dotfiles/nvim/lua/settings.lua
+++ dotfiles/nvim/lua/settings.lua
@@ -30,7 +30,11 @@ vim.filetype.add({
 vim.treesitter.language.register('bash', 'sh')
 vim.treesitter.language.register('bash', 'shell')
 vim.treesitter.language.register('bash', 'text')
+vim.treesitter.language.register('bash', 'robots')
 vim.treesitter.language.register('bash', 'fallback')
+vim.treesitter.language.register('bash', 'pem')
+vim.treesitter.language.register('bash', 'xml')
+vim.treesitter.language.register('bash', 'tmux')
 
 
 opt.completeopt = "menu,menuone,noselect"
