ba26846bf346bf0bf27d2af34f13869c94bf1b3c
ndom91 <yo@ndo.dev>
2024-11-22 17:18:23 +0100

    fix: terraform lsp

--- dotfiles/nvim/lua/plugins/lsp/langs/terraform.lua
+++ dotfiles/nvim/lua/plugins/lsp/langs/terraform.lua
@@ -1,4 +1,4 @@
 -- https://github.com/hashicorp/terraform-ls
-require("lspconfig").terraform_ls.setup({
+require("lspconfig").terraformls.setup({
   capabilities = require("plugins.lsp.capabilities"),
 })
