ca63dd265f52cdb6bd3d17fe8a9a72a8d4e3e12c
ndom91 <yo@ndo.dev>
2025-03-10 17:34:05 +0100

    fix: cleanup zathura config

--- modules/home-manager/zathura.nix
+++ modules/home-manager/zathura.nix
@@ -5,12 +5,8 @@
   programs.zathura = {
     enable = true;
     options = {
-      # recolor-lightcolor = "rgba(0,0,0,0)";
-      # default-bg = "rgba(0,0,0,0.7)";
-
       font = "Inter 12";
       selection-notification = true;
-
       selection-clipboard = "clipboard";
       adjust-open = "best-fit";
       pages-per-row = "1";
@@ -18,19 +14,6 @@
       scroll-full-overlap = "0.01";
       scroll-step = "100";
       zoom-min = "10";
-    };
-
-    # extraConfig = "include catppuccin-mocha";
-  };
-
-  xdg.configFile = {
-    # "zathura/catppuccin-latte".source = pkgs.fetchurl {
-    #   url = "https://raw.githubusercontent.com/catppuccin/zathura/main/src/catppuccin-latte";
-    #   hash = "sha256-nb0ZiHJ9zwlmpN/iHKm3/eRmx4se1om3qCVrfge8B8c=";
-    # };
-    "zathura/catppuccin-mocha".source = pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/catppuccin/zathura/main/src/catppuccin-mocha";
-      hash = "sha256-POxMpm77Pd0qywy/jYzZBXF/uAKHSQ0hwtXD4wl8S2Q=";
     };
   };
 }
