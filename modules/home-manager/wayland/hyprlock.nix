dff625d032bf4c1821330e93545099d97fbbd1e4
ndom91 <yo@ndo.dev>
2025-04-06 14:18:45 +0200

    fix: hyprlock use fingerprint

--- modules/home-manager/wayland/hyprlock.nix
+++ modules/home-manager/wayland/hyprlock.nix
@@ -11,9 +11,11 @@ in
         disable_loading_bar = true;
         immediate_render = true;
       };
-      auth = {
-        fingerprint.enabled = true;
-      };
+
+      # Oddly it only works if this is disabled :thinking:
+      # auth = {
+      #   fingerprint.enabled = true;
+      # };
 
       background = [
         {
@@ -24,47 +26,6 @@ in
           brightness = 0.5;
         }
       ];
-
-      # input-field = [
-      #   {
-      #     monitor = "";
-      #     size = "200, 50";
-      #     position = "0, -120";
-      #     dots_center = true;
-      #     fade_on_empty = true;
-      #     fade_timeout = 5000;
-      #     font_color = "rgb(202, 211, 245)";
-      #     inner_color = "rgb(91, 96, 120)";
-      #     outer_color = "rgb(24, 25, 38)";
-      #     outline_thickness = 5;
-      #     rounding = 15;
-      #     placeholder_text = ''<span font_family="${font_family}" foreground='##cad3f5'>Password...</span>'';
-      #     fail_color = "rgb(204, 34, 34)";
-      #     fail_text = "$FAIL <b>($ATTEMPTS)</b>";
-      #     fail_transition = 500;
-      #   }
-      # ];
-      #
-      # label = [
-      #   {
-      #     monitor = "";
-      #     text = "$TIME";
-      #     inherit font_family;
-      #     font_size = 50;
-      #     position = "0, 150";
-      #     valign = "center";
-      #     halign = "center";
-      #   }
-      #   {
-      #     monitor = "";
-      #     text = "cmd[update:3600000] date +'%A %B %d'";
-      #     inherit font_family;
-      #     font_size = 20;
-      #     position = "0, 50";
-      #     valign = "center";
-      #     halign = "center";
-      #   }
-      # ];
     };
   };
 }
